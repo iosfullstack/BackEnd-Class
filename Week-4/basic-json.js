24828624ceea317dff5e1888e773bb97af48bfe7
iosfullstack <isadare001@gmail.com>
2022-09-14 03:59:57 -0700

    new commits

--- Week-4/basic-json.js
+++ Week-4/basic-json.js
@@ -2,12 +2,47 @@ const express = require(`express`)
 const data = require(`./data`)
 
 const app = express()
+app.use(express.json())
 
 app.get(`/`, (req, res) => {
     res.status(200).send(`HOME`)
 })
+
+// CRUD - HTTP VERBS
 app.get(`/data`, (req, res) => {
     res.status(200).send(data)
+})  
+// Read Operation
+
+// app.post(`/data`, (req, res) => {
+//     console.log(req.body)
+//     res.status(200).send(`data`)
+// })
+
+app.post(`/data`, (req, res) => {
+    const person = req.body
+    data.push(person)
+    res.status(201).send(data)
+})
+
+app.get(`/data/:personId`, (req, res) => {
+    const id = req.params.personId
+    const onePerson = data.find((person) => person.id == id)
+    res.status(200).send(onePerson)
+})
+
+app.put(`/data/:personId`, (req, res) => {
+    const payload = req.body
+    const id = req.params.personId
+    const onePerson = data.find((person) => person.id == id)
+    onePerson.name = payload.name
+    res.status(200).send(onePerson)
+})
+
+app.delete(`/data/:personId`, (req, res) => {
+    const id = req.params.personId
+    const onePerson = data.filter((person) => person.id !== Number(id))
+    res.status(200).send(onePerson)
 })
 
 
--- Week-4/data.js
+++ Week-4/data.js
@@ -1,7 +1,12 @@
 const people = [
     {"id": 1, "name": "Isadare Samson"},
     {"id": 2, "name": "Isadare Gbenga"},
-    {"id": 3, "name": "Isadare Emmanuel"}
+    {"id": 3, "name": "Isadare Emmanuel"},
+    {"id": 4, "name": "Bashir"},
+    {"id": 5, "name": "Anthony"},
+    {"id": 6, "name": "Oliver"},
+    {"id": 7, "name": "Samuel"},
+    {"id": 8, "name": "Hilary"}
 ]
 
 module.exports = people
